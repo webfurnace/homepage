e6858f01f61863b550ded6602fc219426bdc0fd8
unknown <ian.richard@webfurnace.com>
2021-07-26 16:20:07 -0500

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing this Change!
+          Testing this Change
         </a>
       </header>
     </div>
